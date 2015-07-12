9736acd0ce85bc8ee99f74057eed2f2c77c7ccb2
Alessio Delmonti <alessio.d@gmail.com>
2015-07-12 19:35:59 +0200

    some cool stuff

--- dotsoff/www/js/services/Game.svc.js
+++ dotsoff/www/js/services/Game.svc.js
@@ -9,10 +9,20 @@
 		
 		var game = {
 			lvls: getLevels(),
-			setLevel: setLevel
+			setLevel: setLevel,
+			isSolved: isSolved,
+			settings: Settings(),
+			generate: generate,
+			move: move
 		};
 
 		return game;
+
+		function Settings(){
+			return {
+				level: 0
+			};
+		}
 
 		function getLevels(){
 			var lvls = [];
@@ -25,11 +35,255 @@
 					[ 0, 0, 0, 0, 0 ]
 				]
 			});
+
+			lvls.push( { 
+				schema : [
+					[ 1, 0, 0, 0 ,0 ],
+					[ 0, 1, 0, 0, 0 ],
+					[ 0, 0, 1, 0, 0 ],
+					[ 0, 0, 0, 1, 0 ],
+					[ 0, 0, 0, 0, 1 ]
+				]
+			});
+
+			lvls.push( { 
+				schema : [
+					[ 1, 1, 0, 1 ,1 ],
+					[ 1, 0, 1, 0, 1 ],
+					[ 0, 1, 1, 1, 0 ],
+					[ 1, 0, 1, 0, 1 ],
+					[ 1, 1, 0, 1, 1 ]
+				]
+			});
+
+			lvls.push( { 
+				schema : [
+					[ 0, 1, 0, 1 ,0 ],
+					[ 1, 1, 0, 1, 1 ],
+					[ 0, 1, 0, 1, 0 ],
+					[ 1, 0, 1, 0, 1 ],
+					[ 1, 0, 1, 0, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 0, 0, 0 ,1 ],
+					[ 1, 1, 0, 1, 1 ],
+					[ 0, 0, 1, 0, 0 ],
+					[ 1, 0, 1, 0, 0 ],
+					[ 1, 0, 1, 1, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 1, 0, 1 ,1 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 1, 1, 0, 1, 1 ],
+					[ 0, 0, 0, 0, 1 ],
+					[ 1, 1, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 1, 1, 1, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 1, 1 ],
+					[ 0, 0, 0, 1, 1 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 1, 1, 0, 0, 0 ],
+					[ 1, 1, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 1, 1, 1, 0 ],
+					[ 1, 1, 1, 1, 1 ],
+					[ 0, 1, 1, 1, 0 ],
+					[ 0, 0, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0 ,0 ],
+					[ 0, 1, 1, 1, 0 ],
+					[ 0, 1, 1, 1, 0 ],
+					[ 0, 1, 1, 1, 0 ],
+					[ 0, 0, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 1, 0, 1, 1 ],
+					[ 1, 1, 0, 1, 1 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 1, 1, 0, 1, 1 ],
+					[ 1, 1, 0, 1, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 0, 0, 0, 1 ],
+					[ 1, 0, 0, 0, 1 ],
+					[ 1, 0, 0, 0, 1 ],
+					[ 1, 1, 1, 1, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 1, 1, 1 ],
+					[ 0, 0, 0, 1, 1 ],
+					[ 1, 0, 0, 0, 1 ],
+					[ 1, 1, 0, 0, 0 ],
+					[ 1, 1, 1, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0 ,0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 0, 0, 0, 1 ],
+					[ 0, 1, 0, 1, 0 ],
+					[ 0, 0, 1, 0, 0 ],
+					[ 0, 1, 0, 1, 0 ],
+					[ 1, 0, 0, 0, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 0, 1, 0, 1 ],
+					[ 1, 1, 1, 1, 1 ],
+					[ 1, 0, 1, 0, 1 ],
+					[ 1, 1, 1, 1, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0, 1 ],
+					[ 0, 0, 0, 1, 1 ],
+					[ 0, 0, 1, 0, 1 ],
+					[ 0, 1, 0, 0, 1 ],
+					[ 1, 1, 1, 1, 1 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0 ,0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0 ,0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ]
+				]
+			});
+			lvls.push( { 
+				schema : [
+					[ 0, 0, 0, 0 ,0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ],
+					[ 0, 0, 0, 0, 0 ]
+				]
+			});
 			return lvls;
 		}
 
-		function setLevel( ){
-			return game.lvls[0];
+		function setLevel( level ){
+			if( game.lvls[level] !== undefined) return game.lvls[level];
+			return {
+				schema: generate()
+			};
+		}
+
+		function generate(){
+			var schema = [];
+			for(var i = 0; i<5; i++){
+				schema[i] = [];
+				for(var y = 0; y<5; y++){
+					var val = Math.round(Math.random());
+					schema[i][y] = val;
+				}
+			}
+			return schema;
+
+		}
+
+		function move(y, x, schema){
+			var rows = schema;
+
+			if( rows[y][x] == 1 ) rows[y][x] = 0;
+			else rows[y][x] = 1;
+			//Up
+			if(rows[(y+1)] !== undefined){
+				if( rows[(y+1)][x] == 1 ) rows[(y+1)][x] = 0;
+				else rows[(y+1)][x] = 1;
+			}
+			//Down
+			if(rows[(y-1)] !== undefined){
+				if( rows[y-1][x] == 1 ) rows[y-1][x] = 0;
+				else rows[y-1][x] = 1;
+			}
+			//Right
+			if(rows[y][(x+1)] !== undefined){
+				if( rows[y][x+1] == 1 ) rows[y][x+1] = 0;
+				else rows[y][x+1] = 1;
+			}
+			//Left
+			if(rows[y][(x-1)] !== undefined){
+				if( rows[y][x-1] == 1 ) rows[y][x-1] = 0;
+				else rows[y][x-1] = 1;
+			}
+			return schema;
+		}
+
+		function canBeSolved(schema){
+			console.log(schema);
+			for(var i = 0; i<schema.length; i++){
+				var string = '';
+				for(var y = 0; y<schema[i].length; y++){
+					string += ', '+schema[i][y];
+					if( schema[i][y] > 0){
+						var t = move(i, y, schema);	
+						//return canBeSolved(t)
+						break;
+					}
+				
+				}
+				console.log(string);
+			}
+		}
+
+		function isSolved(schema){
+			for(var i = 0; i<schema.length; i++)	
+				for(var y = 0; y<schema[i].length; y++)	
+					if( schema[i][y] > 0)
+						return false
+
+			return true;
 		}
 	}
 })();
