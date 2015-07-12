7215b464fa6eb72ffaea3a5bd83b7dfaa538d80e
Alessio Delmonti <alessio.d@gmail.com>
2015-07-12 16:16:57 +0200

    engine

--- js/controllers/GameCtrl.js
+++ js/controllers/GameCtrl.js
@@ -5,7 +5,7 @@
 	.module('dotsoff')
 	.controller('GameCtrl', GameCtrl);
 
-	function GameCtrl($rootScope, Game){
+	function GameCtrl($rootScope, Game, $timeout){
 		var vm = this;
 		vm.showStartScreen = true;
 
@@ -15,5 +15,7 @@
 			vm.showStartScreen = false;
 			$rootScope.$broadcast('game.play');
 		}
+		
+		$timeout(start, 150);
 	}
 })();
