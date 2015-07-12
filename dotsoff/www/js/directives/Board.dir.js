ab616f475c4e52fe1ad90ed7f404b654af820876
Alessio Delmonti <alessio.d@gmail.com>
2015-07-12 16:16:57 +0200

    engine

--- dotsoff/www/js/directives/Board.dir.js
+++ dotsoff/www/js/directives/Board.dir.js
@@ -5,21 +5,21 @@
 	.module('dotsoff')
 	.directive('board', Board);
 
-	function Board(){
+	function Board($compile){
 		return {
 			restrict: 'E',
 			template: [
 				'<div class="board">',
-					'<div class="clear" ng-repeat="row in rows track by $index">',
-						'<dot ng-repeat="(k, v) in rows[$index] track by k" data-on="{{v}}">{{v}}</dot>',
+					'<div class="clear" ng-repeat="row in rows.schema track by $index">',
+						'<dot ng-repeat="(k, v) in rows.schema[$index] track by k" row="{{$parent.$index}}" col="{{k}}" on="{{v}}"></dot>',
 					'</div>',
 				'</div>'
 			].join(''),
-			
-			controller: function($rootScope, $scope, $element, Game){
+			controller: function($rootScope, $scope, $element, Game, $compile, $timeout){
 				var board = $element[0];
 				$scope.dots = [];
 				$scope.render = render;
+				this.calculateAdjacent = calculateAdjacent;
 
 				$rootScope.$on('game.play', startLevel);
 				
@@ -29,8 +29,38 @@
 				}
 
 				function render(){
-					$scope.rows = Game.setLevel().schema;
-					console.log( $scope.rows);
+					$scope.rows = Game.setLevel();
+				}
+
+				function calculateAdjacent(y, x){
+					var rows = $scope.rows.schema;
+
+					if( rows[y][x] == 1 ) rows[y][x] = 0;
+					else rows[y][x] = 1;
+					//Up
+					if(rows[(y+1)] !== undefined){
+						if( rows[(y+1)][x] == 1 ) rows[(y+1)][x] = 0;
+						else rows[(y+1)][x] = 1;
+					}
+					//Down
+					if(rows[(y-1)] !== undefined){
+						if( rows[y-1][x] == 1 ) rows[y-1][x] = 0;
+						else rows[y-1][x] = 1;
+					}
+					//Right
+					if(rows[y][(x+1)] !== undefined){
+						if( rows[y][x+1] == 1 ) rows[y][x+1] = 0;
+						else rows[y][x+1] = 1;
+					}
+					//Left
+					if(rows[y][(x-1)] !== undefined){
+						if( rows[y][x-1] == 1 ) rows[y][x-1] = 0;
+						else rows[y][x-1] = 1;
+					}
+
+					$scope.$apply(function(){
+						$scope.rows = { schema: rows };
+					});
 				}
 			}
 		};
--- dotsoff/www/js/directives/Dot.dir.js
+++ dotsoff/www/js/directives/Dot.dir.js
@@ -7,24 +7,37 @@
 
 	function Dot(){
 		return {
+			require: '^board',
 			restrict: 'E',
 			template: [
 				'<div class="dot">',
+					//'{{row}},{{col}} - {{val}}',
 				'</div>'
 			].join(''),
-
 			replace: true,
+			link: function(scope, elem, attrs, boardCtrl) {
+				scope.col = attrs.col;
+				scope.row = attrs.row;
+				scope.boardCtrl = boardCtrl;
 
-			link: function(scope, elem, attrs) {
-				if( attrs.on == '1') elem.css('background-color', 'white');
+				attrs.$observe('on', function(value){
+					if( value == '1') elem.css('background-color', 'white');
+					else elem.css('background-color', 'red');
+					scope.val = attrs.on;
+				});
 			},
-			controller: function($rootScope, $scope, $element){
+			controller: function($rootScope, $scope, $element, $attrs){
 				var dot = $element[0];
 
 				dot.addEventListener('click', click, false);
-
+				
 				function click(){
-					console.log('cliiiiiiiiik');
+					var col = parseInt( $attrs.col, 10);
+					var row = parseInt( $attrs.row, 10);
+					//console.log( 'col: '+$attrs.col );
+					//console.log( 'row: '+$attrs.row );
+					//console.log( $attrs.on );
+					$scope.boardCtrl.calculateAdjacent( row, col );
 				}
 			}
 		};
