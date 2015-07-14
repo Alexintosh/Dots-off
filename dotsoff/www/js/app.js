52ce7c289cd2cb076094ec4c0b78714fc966d44f
Alessio Delmonti <alessio.d@gmail.com>
2015-07-14 08:58:26 +0200

    ionic core services

--- dotsoff/www/js/app.js
+++ dotsoff/www/js/app.js
@@ -1,14 +1,18 @@
-angular.module('dotsoff', ['ionic'])
-
-.run(function($ionicPlatform) {
+angular.module('dotsoff', ['ionic', 'ionic.service.core', 'ionic.service.analytics'])
+.config(function($ionicAppProvider){
+	$ionicAppProvider.identify({
+		app_id: 'bc4d9080',
+		api_key: '26f2c1cf96579d758cbaa9c2018f99d97689df12ac4e01f5'
+	});
+})
+.run(function($ionicPlatform, $ionicAnalytics) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+	$ionicAnalytics.register();
+	if(window.cordova && window.cordova.plugins.Keyboard) {
+		cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	}
+	if(window.StatusBar) {
+		StatusBar.styleDefault();
+	}
   });
 })
